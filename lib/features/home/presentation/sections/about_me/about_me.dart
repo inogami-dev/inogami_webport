dd5e89672013c3ab2a433c7a4e8883844c3bde70
inogami <dhetterjan23@gmail.com>
2026-09-15 23:48:00 +0800

    refactor: changed the initial layout of About Me section's left side contents.

--- lib/features/home/presentation/sections/about_me/about_me.dart
+++ lib/features/home/presentation/sections/about_me/about_me.dart
@@ -16,76 +16,59 @@ class MyAboutMeSection extends StatelessWidget {
     final double leftSideContents = width * 0.32;
 
     return Padding(
-      padding: const EdgeInsets.only(left: 48),
+      padding: const EdgeInsets.only(left: 48, right: 48),
       child: Row(
         children: [
-          SizedBox(
+          // Left Side Contents
+          Container(
             width: leftSideContents,
             height: height,
-            child: Stack(
-              alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(top: 24, right: 16),
+            child: Column(
+              spacing: 16,
               children: [
-                Container(
-                  width: width * 0.5,
-                  height: height * 0.5,
-                  // color: Colors.amber,
-                  // margin: EdgeInsets.only(top: 32),
-                  child: AspectRatio(
-                    aspectRatio: 1 / 1,
-                    child: Image.asset("assets/images/me2.webp"),
-                  ),
+                CircleAvatar(
+                  backgroundColor: Colors.amber,
+                  radius: 40,
+                  backgroundImage: AssetImage("assets/images/me3.jpg"),
                 ),
-                Positioned(
-                  bottom: 0,
-                  child: Container(
-                    width: leftSideContents,
-                    height: height * 0.45,
-                    padding: EdgeInsets.only(top: 32, left: 32, right: 32),
-                    decoration: BoxDecoration(
-                      color: myColorScheme.surfaceContainer,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(32),
-                        topRight: Radius.circular(32),
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 8,
-                      children: [
-                        MyText(
-                          text: "Inogami",
-                          fontFamily: "Poppins",
-                          fontSize: kDefaultFontSize + 8,
-                          fontWeight: FontWeight.w600,
-                        ),
-                        MyText(
-                          text:
-                              "whdakj akjdbajd wjkabdwjk akjdawj wjkdbawkdjb awjdbakwjbd whdakj akjdbajd wjkabdwjk akjdawj wjkdbawkdjb awjdbakwjbd",
-                          maxLines: 56,
-                        ),
 
-                        // Quote
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              MyText(
-                                text:
-                                    "\"The only way to do great work is to love what you do\"",
-                                maxLines: 56,
-                              ),
-                              MyText(text: "-Steve Jobs"),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                MyText(
+                  text: "Inogami",
+                  fontFamily: "Poppins",
+                  fontSize: kDefaultFontSize + 8,
+                  fontWeight: FontWeight.w600,
+                ),
+                MyText(
+                  text:
+                      "whdakj akjdbajd wjkabdwjk akjdawj wjkdbawkdjb awjdbakwjbd whdakj akjdbajd wjkabdwjk akjdawj wjkdbawkdjb awjdbakwjbd",
+                  maxLines: 56,
+                ),
+
+                // Quote
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      MyText(
+                        text:
+                            "\"The only way to do great work is to love what you do\"",
+                        maxLines: 56,
+                      ),
+                      MyText(text: "- Steve Jobs"),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-          Column(children: []),
+
+          // Right Side Contents
+          Expanded(
+            child: Column(
+              children: [Expanded(child: Container(color: Colors.amber))],
+            ),
+          ),
         ],
       ),
     );
