f954e075528a72236223cadaad71ea206acc5346
inogami <dhetterjan23@gmail.com>
2026-09-17 00:58:04 +0800

    chore: separated the left side contents of the About Me section into its own StatelessWidget.

--- lib/features/home/presentation/sections/about_me/about_me.dart
+++ lib/features/home/presentation/sections/about_me/about_me.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/core/widgets/text.dart';
-import 'package:my_portfolio/core/widgets/text_rich.dart';
+import 'package:my_portfolio/features/home/presentation/sections/about_me/widgets/left_side_contents.dart';
 
 class MyAboutMeSection extends StatelessWidget {
   final double width;
@@ -21,84 +21,32 @@ class MyAboutMeSection extends StatelessWidget {
       child: Row(
         children: [
           // Left Side Contents
-          Container(
-            width: leftSideContents,
+          AboutMeLeftSideContent(
+            leftSideContents: leftSideContents,
             height: height,
-            padding: EdgeInsets.only(top: 16, right: 16, bottom: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 16,
-              children: [
-                // Center(
-                //   child: Column(
-                //     children: [
-                CircleAvatar(
-                  backgroundColor: Colors.amber,
-                  radius: 40,
-                  backgroundImage: AssetImage("assets/images/me3.jpg"),
-                ),
-                // SizedBox(height: 16),
-
-                MyText(
-                  text: "Inogami",
-                  fontFamily: "Poppins",
-                  fontSize: kDefaultFontSize + 8,
-                  fontWeight: FontWeight.w600,
-                ),
-                //     ],
-                //   ),
-                // ),
-
-                MyText(
-                  text:
-                      "Senior IT student at UM Tagum College, specializing in mobile app development with Flutter.",
-                  fontWeight: FontWeight.w500,
-                  maxLines: 56,
-                ),
-
-                MyTextRich(
-                  children: [
-                    TextSpan(
-                      text: "Driven by a simple philosophy: ",
-                      style: TextStyle(fontWeight: FontWeight.w600),
-                    ),
-
-                    TextSpan(
-                      text:
-                          "intuitive functionality paired with clean, elegant UI and fluid interactions.",
-                    ),
-                  ],
-                ),
-
-                MyTextRich(
-                  children: [
-                    TextSpan(
-                      text: "Beyond the screen: ",
-                      style: TextStyle(fontWeight: FontWeight.w600),
-                    ),
-
-                    TextSpan(
-                      text:
-                          "A creative thinker and proactive builder who thrives in goal-driven environments, motivated to create high-impact software with teams that constantly push for greatness.",
-                    ),
-                  ],
-                ),
-
-                // Personnal Motto
-                MyText(
-                  text: "Transforming chaos into elegant solutions",
-                  maxLines: 56,
-                  fontStyle: FontStyle.italic,
-                ),
-              ],
-            ),
           ),
 
           // Right Side Contents
           Expanded(
             child: Column(
-              children: [Expanded(child: Container(color: Colors.amber))],
+              children: [
+                Container(
+                  width: width * 0.3,
+                  height: height * 0.2,
+                  color: Colors.grey,
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MyText(text: "Course", fontSize: kDefaultFontSize + 8),
+                      MyText(text: "School"),
+                      MyText(text: "Year"),
+                      MyText(text: "Achievements"),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
---
+++ lib/features/home/presentation/sections/about_me/widgets/left_side_contents.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/core/widgets/text.dart';
+import 'package:my_portfolio/core/widgets/text_rich.dart';
+
+class AboutMeLeftSideContent extends StatelessWidget {
+  const AboutMeLeftSideContent({
+    super.key,
+    required this.leftSideContents,
+    required this.height,
+  });
+
+  final double leftSideContents;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: leftSideContents,
+      height: height,
+      padding: EdgeInsets.only(top: 8, right: 16, bottom: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 16,
+        children: [
+          // Center(
+          //   child: Column(
+          //     children: [
+          CircleAvatar(
+            backgroundColor: Colors.amber,
+            radius: 40,
+            backgroundImage: AssetImage("assets/images/me3.jpg"),
+          ),
+          // SizedBox(height: 16),
+
+          MyText(
+            text: "Inogami",
+            fontFamily: "Poppins",
+            fontSize: kDefaultFontSize + 8,
+            fontWeight: FontWeight.w600,
+          ),
+          //     ],
+          //   ),
+          // ),
+
+          MyText(
+            text:
+                "Senior IT student at UM Tagum College, specializing in mobile app development with Flutter.",
+            fontWeight: FontWeight.w500,
+            maxLines: 56,
+          ),
+
+          MyTextRich(
+            children: [
+              TextSpan(
+                text: "Driven by a simple philosophy: ",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+
+              TextSpan(
+                text:
+                    "intuitive functionality paired with clean, elegant UI and fluid interactions.",
+              ),
+            ],
+          ),
+
+          MyTextRich(
+            children: [
+              TextSpan(
+                text: "Beyond the screen: ",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+
+              TextSpan(
+                text:
+                    "A creative thinker and proactive builder who thrives in goal-driven environments, motivated to create high-impact software with teams that constantly push for greatness.",
+              ),
+            ],
+          ),
+
+          // Personnal Motto
+          MyText(
+            text: "Transforming chaos into elegant solutions",
+            maxLines: 56,
+            fontStyle: FontStyle.italic,
+          ),
+        ],
+      ),
+    );
+  }
+}
