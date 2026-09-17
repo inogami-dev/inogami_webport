4dcec3c7cbf463279c34dd73c6ed2c034407d7e0
inogami <dhetterjan23@gmail.com>
2026-09-18 06:48:47 +0800

    refactor: separated the education contents into its own widget called MiddleContent.

--- lib/features/home/presentation/sections/about_me/about_me.dart
+++ lib/features/home/presentation/sections/about_me/about_me.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/core/widgets/text.dart';
-import 'package:my_portfolio/features/home/presentation/sections/about_me/widgets/education_card.dart';
-import 'package:my_portfolio/core/widgets/floating_animation.dart';
 import 'package:my_portfolio/features/home/presentation/sections/about_me/widgets/left_side_contents.dart';
+import 'package:my_portfolio/features/home/presentation/sections/about_me/widgets/middle_content.dart';
 
 class MyAboutMeSection extends StatelessWidget {
   final double width;
@@ -28,42 +26,8 @@ class MyAboutMeSection extends StatelessWidget {
             height: height,
           ),
 
-          // Right Side Contents
-          SizedBox(
-            width: width / 3,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 16,
-              children: [
-                MyText(text: "Education"),
-
-                EducationCard(
-                  width: width,
-                  // height: height,
-                  course: 'BS Information Technology',
-                  school: 'UM Tagum College',
-                  year: '2022-2027',
-                  achievements: [
-                    "Consistent Dean's Lister",
-                    "4th/16 in Hackathon Challenge 2026",
-                  ],
-                ),
-                EducationCard(
-                  cardEntryNumber: 2,
-                  width: width,
-                  // height: height,
-                  course: 'Computer System Servicing (CSS)',
-                  school: 'Lorenzo S. Sarmiento Sr. NHS',
-                  year: '2020-2022',
-                  achievements: [
-                    "With High Honors",
-                    "Rank 5th in Overall Honors",
-                  ],
-                ),
-              ],
-            ),
-          ),
+          // Middle Contents
+          MiddleContent(width: width),
           Expanded(child: Container(color: Colors.amber)),
         ],
       ),
---
+++ lib/features/home/presentation/sections/about_me/widgets/middle_content.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/core/widgets/text.dart';
+import 'package:my_portfolio/features/home/presentation/sections/about_me/widgets/education_card.dart';
+
+class MiddleContent extends StatelessWidget {
+  const MiddleContent({super.key, required this.width});
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width / 3,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 16,
+        children: [
+          MyText(text: "Education"),
+
+          EducationCard(
+            width: width,
+            // height: height,
+            course: 'BS Information Technology',
+            school: 'UM Tagum College',
+            year: '2022-2027',
+            achievements: [
+              "Consistent Dean's Lister",
+              "4th/16 in Hackathon Challenge 2026",
+            ],
+          ),
+          EducationCard(
+            cardEntryNumber: 2,
+            width: width,
+            // height: height,
+            course: 'Computer System Servicing (CSS)',
+            school: 'Lorenzo S. Sarmiento Sr. National High School',
+            year: '2020-2022',
+            achievements: ["With High Honors", "Rank 5th in Overall Honors"],
+          ),
+        ],
+      ),
+    );
+  }
+}
